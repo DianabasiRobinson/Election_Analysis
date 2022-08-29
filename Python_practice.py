6f053a1cd9551e0e6cc52b8149517e413fedd4d8
RobinsonDiana <dianabasirobinson1@gmail.com>
2022-08-28 23:27:21 -0400

    Adding Pypoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,31 @@
+print("Hello World")
+print("I am Using Python")
+# Import the datetime class from the datetime module.
+import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+import csv
+dir(csv)
+import csv
+import os
+
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     print(election_data)
+     
+     # Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Use the open statement to open the file as a text file.
+outfile = open(file_to_save, "w")
+# Write some data to the file.
+outfile.write("Hello World")
+
+# Close the file
+outfile.close()
